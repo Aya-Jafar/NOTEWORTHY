555f201032a20914e8b560672c9864797fefae1f
Aya-jafar <aya.jafar002@gmail.com>
2024-08-19 02:06:38 +0300

    Add letters to piano & style changes

--- src/components/Piano.jsx
+++ src/components/Piano.jsx
@@ -4,17 +4,31 @@ function Piano() {
   // Define the number of keys and their layout
   const totalKeys = 61;
 
-  // Generate boolean array representing the keys colors
-  const keys = Array.from({ length: totalKeys }, (_, index) => index % 2 === 0);
+  // Define the layout of keys using keyboard letters
+  const keyLetters = 'QWERTYUIOPASDFGHJKLZXCVBNM';
+
+  // Generate the layout for keys based on the keyboard letters
+  const keys = Array.from({ length: totalKeys }, (_, index) => {
+    const letterIndex = index % keyLetters.length;
+    return keyLetters[letterIndex];
+  });
 
   return (
     <div className="pianoPage">
       <div className="piano">
-        {keys.map((key, index) => (
-          <div className={key ? "white-key" : "black-key"} key={index}>
-            <p className={key ? "white-key-text" : "black-key-text"}>HI</p>
-          </div>
-        ))}
+        {keys.map((key, index) => {
+          const isWhiteKey = index % 2 === 0;
+          return (
+            <div
+              className={isWhiteKey ? "white-key" : "black-key"}
+              key={index}
+            >
+              <p className={isWhiteKey ? "white-key-text" : "black-key-text"}>
+                {key}
+              </p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
